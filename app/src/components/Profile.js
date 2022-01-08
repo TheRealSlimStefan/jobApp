2637857d3a4ae6f934d99a45054047e80d3a96fb
TheRealSlimStefan <19smarcins98@gmail.com>
2022-01-08 17:44:25 +0100

    auth update

--- app/src/components/Profile.js
+++ app/src/components/Profile.js
@@ -1,18 +1,31 @@
 import "../styles/Profile.css";
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { BiLogOut } from "react-icons/bi";
 import { AiFillEdit } from "react-icons/ai";
 import { useAuth } from "../contexts/AuthContext";
 import profileImg from "../images/default_image.png";
+import { db } from "../firebase";
 
 function Profile() {
     let navigate = useNavigate();
     const { logOut } = useAuth();
 
+    const [messages, setMessages] = useState();
+
     async function logOutUser() {
         await logOut();
         navigate("/login");
     }
+
+    useEffect(() => {
+        db.collection("messages")
+            .orderBy("createdAt")
+            .limit(50)
+            .onSnapshot((snapshot) => {
+                setMessages(snapshot.docs.map((doc) => doc.data()));
+            });
+    }, []);
 
     return (
         <>
@@ -38,7 +51,14 @@ function Profile() {
                     <AiFillEdit />
                     <p>Edutuj profil</p>
                 </a>
-                <div></div>
+                <div>
+                    {messages.map(({ id, text, photoUrl }) => {
+                        <div key={id}>
+                            <img src={photoUrl} />
+                            <p>{text}</p>
+                        </div>;
+                    })}
+                </div>
             </div>
         </>
     );
