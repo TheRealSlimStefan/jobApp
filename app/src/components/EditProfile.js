e699061e001998e42c29c29b29ac98b918fc6897
TheRealSlimStefan <19smarcins98@gmail.com>
2022-01-18 14:00:53 +0100

    repairing some styles

--- app/src/components/EditProfile.js
+++ app/src/components/EditProfile.js
@@ -1,52 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import { db } from '../firebase';
-import {
-    doc,
-    getDoc,
-    updateDoc
+import React, { useState, useEffect } from "react";
+import { db } from "../firebase";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { useAuth } from "../contexts/AuthContext";
 
-} from 'firebase/firestore';
-import { useAuth } from '../contexts/AuthContext';
-
-import '../styles/EditProfile.css';
-import Select from 'react-select';
-import { AiOutlineArrowLeft } from 'react-icons/ai';
-import { Link} from 'react-router-dom';
+import "../styles/EditProfile.css";
+import Select from "react-select";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 export default function EditProfile() {
     // Selector options
     const workingHoursOptions = [
-        { value: '1/1', label: 'Pełen etat' },
-        { value: '3/4', label: '3/4 Etatu' },
-        { value: '1/2', label: '1/2 Etatu' },
+        { value: "1/1", label: "Pełen etat" },
+        { value: "1/2", label: "1/2 Etatu" },
+        { value: "3/4", label: "3/4 Etatu" },
     ];
     const localiztionOptions = [
-        { value: 'Podlaskie', label: 'Podlaskie' },
-        { value: 'Białystok', label: 'Białystok' },
-        { value: 'Warszawa', label: 'Warszawa' },
+        { value: "Podlaskie", label: "Podlaskie" },
+        { value: "Białystok", label: "Białystok" },
+        { value: "Warszawa", label: "Warszawa" },
     ];
     const expirienceOptions = [
-        { value: 'intern', label: 'Stażysta' },
-        { value: 'junior', label: 'Junior' },
-        { value: 'mid', label: 'Mid' },
-        { value: 'senior', label: 'Senior' },
+        { value: "intern", label: "Stażysta" },
+        { value: "junior", label: "Junior" },
+        { value: "mid", label: "Mid" },
+        { value: "senior", label: "Senior" },
     ];
     const technologiesOptions = [
-        { value: 'HTML', label: 'HTML' },
-        { value: 'CSS', label: 'CSS' },
-        { value: 'JS', label: 'JS' },
-        { value: 'Java', label: 'Java' },
-        { value: 'C#', label: 'C#' },
-        { value: 'C++', label: 'C++' },
-        { value: 'Git', label: 'Git' },
+        { value: "HTML", label: "HTML" },
+        { value: "CSS", label: "CSS" },
+        { value: "JS", label: "JS" },
+        { value: "Java", label: "Java" },
+        { value: "C#", label: "C#" },
+        { value: "C++", label: "C++" },
+        { value: "Git", label: "Git" },
     ];
     const positionsOptions = [
-        { value: 'programmer ', label: 'Programista' },
-        { value: 'webDeveloper', label: 'Web Developer' },
-        { value: 'devops', label: 'DevOps' },
-        { value: 'frontendDeveloper', label: 'Front-end Developer' },
-        { value: 'backendDeveloper', label: 'Back-end Developer' },
-        { value: 'fullStackDeveloper', label: 'Full Stack Developer' },
+        { value: "programmer ", label: "Programista" },
+        { value: "webDeveloper", label: "Web Developer" },
+        { value: "devops", label: "DevOps" },
+        { value: "frontendDeveloper", label: "Front-end Developer" },
+        { value: "backendDeveloper", label: "Back-end Developer" },
+        { value: "fullStackDeveloper", label: "Full Stack Developer" },
     ];
 
     const { currentUser } = useAuth();
@@ -67,97 +62,131 @@ export default function EditProfile() {
         updateWorkingHours(id, workingHours);
         updateExperience(id, experience);
         updateDescription(id, description);
-    }
+    };
 
     const updateOccupation = async (id, occupation) => {
-        const occupationValues = occupation.map(el => el.value);
+        const occupationValues = occupation.map((el) => el.value);
         const profileDoc = doc(db, "users", id);
-        const newFields = {occupation: occupationValues};
+        const newFields = { occupation: occupationValues };
         await updateDoc(profileDoc, newFields);
-    }
+    };
 
     const updateTechnology = async (id, technology) => {
-        const technologyValues = technology.map(el => el.value);
+        const technologyValues = technology.map((el) => el.value);
         const profileDoc = doc(db, "users", id);
-        const newFields = {technology: technologyValues};
+        const newFields = { technology: technologyValues };
         await updateDoc(profileDoc, newFields);
-    }
+    };
 
     const updateDescription = async (id, description) => {
         const profileDoc = doc(db, "users", id);
-        const newFields = {description: description};
+        const newFields = { description: description };
         await updateDoc(profileDoc, newFields);
-    }
+    };
 
     const updateExperience = async (id, experience) => {
         const profileDoc = doc(db, "users", id);
-        const newFields = {experience: experience};
+        const newFields = { experience: experience };
         await updateDoc(profileDoc, newFields);
-    }
+    };
 
     const updateLocation = async (id, localization) => {
         const profileDoc = doc(db, "users", id);
-        const newFields = {localization: localization};
+        const newFields = { localization: localization };
         await updateDoc(profileDoc, newFields);
-    }
+    };
     const updateWorkingHours = async (id, workingHours) => {
         const profileDoc = doc(db, "users", id);
-        const newFields = {workingHours: workingHours};
+        const newFields = { workingHours: workingHours };
         await updateDoc(profileDoc, newFields);
-    }
+    };
 
     useEffect(() => {
         async function getProfileData() {
-
-            const queryResponse = await getDoc(doc(db, "users", currentUser.uid));
-            setProfileData(queryResponse.data())
+            const queryResponse = await getDoc(
+                doc(db, "users", currentUser.uid)
+            );
+            setProfileData(queryResponse.data());
         }
 
         getProfileData();
-        console.log(profileData);
+        // console.log(profileData);
     }, []);
 
     return (
         <div className="editProfile">
             <div className="container">
-                <Link className="backBtn" to="/profile">
-                    <AiOutlineArrowLeft />
-                </Link>
+                <div className="buttonContainer">
+                    <Link className="backBtn" to="/profile">
+                        <AiOutlineArrowLeft />
+                    </Link>
+                </div>
                 <form>
                     <div className="field">
-                        <label for="aboutMe">O mnie:</label>
+                        <label htmlFor="aboutMe">O MNIE</label>
                         <textarea
                             type="text"
                             name="peas"
                             id="aboutMe"
-                            onChange={e => {setDescription(e.target.value)}}
+                            onChange={(e) => {
+                                setDescription(e.target.value);
+                            }}
                         ></textarea>
                     </div>
                     <div className="field">
-                        <label for="aboutMe">Stanowisko</label>
-                        <Select options={positionsOptions} isMulti onChange={e => setOccupation([...e])} />
+                        <label htmlFor="aboutMe">STANOWISKO</label>
+                        <Select
+                            options={positionsOptions}
+                            isMulti
+                            onChange={(e) => setOccupation([...e])}
+                        />
                     </div>
                     <div className="field">
-                        <label for="aboutMe">Technologie</label>
-                        <Select options={technologiesOptions} isMulti onChange={e => setTechnology([...e])}/>
+                        <label htmlFor="aboutMe">TECHNOLOGIE</label>
+                        <Select
+                            options={technologiesOptions}
+                            isMulti
+                            onChange={(e) => setTechnology([...e])}
+                        />
                     </div>
                     <div className="field">
-                        <label for="aboutMe">Doświadczenie</label>
-                        <Select options={expirienceOptions} onChange={e => {setExperience(e.value)}} />
+                        <label htmlFor="aboutMe">DOŚWIADCZENIE</label>
+                        <Select
+                            options={expirienceOptions}
+                            onChange={(e) => {
+                                setExperience(e.value);
+                            }}
+                        />
                     </div>
                     <div className="field">
-                        <label for="aboutMe">Lokalizacja</label>
-                        <Select options={localiztionOptions} onChange={e => {setLocalization(e.value)}}/>
+                        <label htmlFor="aboutMe">LOKALIZACJA</label>
+                        <Select
+                            options={localiztionOptions}
+                            onChange={(e) => {
+                                setLocalization(e.value);
+                            }}
+                        />
                     </div>
                     <div className="field">
-                        <label for="">Wymiar pracy</label>
-                        <Select options={workingHoursOptions} onChange={e => {setWorkingHours(e.value)}}/>
+                        <label htmlFor="">WYMIAR PRACY</label>
+                        <Select
+                            options={workingHoursOptions}
+                            onChange={(e) => {
+                                setWorkingHours(e.value);
+                            }}
+                        />
                     </div>
                 </form>
-                <div className='applyBtnContainer'>
-                    <button className="applyBtn" onClick={() => {updateProfile(currentUser.uid)}}>Zapisz zmiany</button>
+                <div className="applyBtnContainer">
+                    <button
+                        className="applyBtn"
+                        onClick={() => {
+                            updateProfile(currentUser.uid);
+                        }}
+                    >
+                        Zapisz zmiany
+                    </button>
                 </div>
-
             </div>
         </div>
     );
--- app/src/components/Profile.js
+++ app/src/components/Profile.js
@@ -1,11 +1,14 @@
-import '../styles/Profile.css';
-import { Link, useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-import { BiLogOut } from 'react-icons/bi';
-import { AiFillSetting } from 'react-icons/ai';
-import { AiFillEdit } from 'react-icons/ai';
-import { useAuth } from '../contexts/AuthContext';
-import profileImg from '../images/default_image.png';
+import "../styles/Profile.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { BiLogOut } from "react-icons/bi";
+import { AiFillSetting } from "react-icons/ai";
+import { AiFillEdit } from "react-icons/ai";
+import { useAuth } from "../contexts/AuthContext";
+import profileImg from "../images/corpo_worker.png";
+import { IoLocationSharp } from "react-icons/io5";
+import { GiSkills } from "react-icons/gi";
+import { MdWork } from "react-icons/md";
 
 function Profile() {
     let navigate = useNavigate();
@@ -13,7 +16,7 @@ function Profile() {
 
     async function logOutUser() {
         await logOut();
-        navigate('/login');
+        navigate("/login");
     }
 
     return (
@@ -23,15 +26,15 @@ function Profile() {
                     <button
                         className="logoutBtn"
                         onClick={() => {
-                            // setUser({});
-                            // navigate("/");
-                            // setLogged(false);
                             logOutUser();
                         }}
                     >
                         <BiLogOut />
                     </button>
-                    <button className="settingsBtn">
+                    <button
+                        className="settingsBtn"
+                        onClick={() => navigate("/editprofile")}
+                    >
                         <AiFillSetting />
                     </button>
                     <img
@@ -39,49 +42,47 @@ function Profile() {
                         className="profileImg"
                         alt="profile"
                     ></img>
-                    <Link
-                        to="/editprofile"
-                        className="edit_profile_btn"
-                        onClick={() => {}}
-                        href="#"
-                    >
-                        <AiFillEdit />
-                        <p>Edutuj profil</p>
-                    </Link>
                 </div>
 
-                <div className="profileInfo">
-                    <div className="info">
-                        <p className="label">O mnie</p>
-                        <p className="content">
-                            Strasznie wczoraj zachlałem. Paliłem blanty do
-                            piątej rano. Film mi się urwał jak leżałem w rurze.
-                            Teraz mnie krzyż napierdala. Trochę się przespałem,
-                            ale musiałem wstać rano bo mam obowiązki. Mam
-                            dziecko. Niektórzy mówią, że nie można chlać jak się
-                            ma dzieci, ale to nie prawda. Można, tylko trzeba
-                            wstawać rano. Na tym polega odpowiedzialność.
+                <div className="moreInfoContainer">
+                    <div className="moreInfoElement">
+                        <h2>O MNIE</h2>
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and
+                            typesetting industry. Lorem Ipsum has been the
+                            industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and
+                            scrambled it to make a type specimen book.
                         </p>
                     </div>
-                    <div className="info">
-                        <p className="label">Stanowisko</p>
-                        <p className="content">React Native specialist</p>
+                    <div className="moreInfoElement">
+                        <h2>TECHNOLOGIE</h2>
+                        <div className="technologiesContainer">
+                            <div className="technology">React</div>
+                            <div className="technology">Javascript</div>
+                            <div className="technology">CSS</div>
+                            <div className="technology">HTML</div>
+                            <div className="technology">Java</div>
+                            <div className="technology">Python</div>
+                        </div>
                     </div>
-                    <div className="info">
-                        <p className="label">Technologie</p>
-                        <p className="content">HTML CSS</p>
+                    <div className="moreInfoElement">
+                        <h2>DOŚWIADCZENIE</h2>
+                        <div className="skillLevelContainer">
+                            <GiSkills /> <p>Junior</p>
+                        </div>
                     </div>
-                    <div className="info">
-                        <p className="label">Doświadczenie</p>
-                        <p className="content">Junior</p>
+                    <div className="moreInfoElement">
+                        <h2>LOKALIZACJA</h2>
+                        <div className="locationContainer">
+                            <IoLocationSharp /> <p>Białystok</p>
+                        </div>
                     </div>
-                    <div className="info">
-                        <p className="label">Lokalizacja</p>
-                        <p className="content">Bialystok</p>
-                    </div>
-                    <div className="info">
-                        <p className="label">Wymiar pracy</p>
-                        <p className="content">3/5 etatu</p>
+                    <div className="moreInfoElement">
+                        <h2>WYMIAR PRACY</h2>
+                        <div className="hoursOfWorkContainer">
+                            <MdWork /> <p>3/5 etatu</p>
+                        </div>
                     </div>
                 </div>
             </div>
